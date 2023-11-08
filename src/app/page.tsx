9fbd799b0c0bf52db1ee6cbdc569e3a36e0f420d
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-11-08 12:38:41 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ type ligiType = {
 export default async function Home() {
   const data = await getFixtures();
   console.log(data.response);
-  const dataActive = data.response.slice(40, 65);
+  const dataActive = data.response.slice(118, 133);
 
   return (
     <div className={styles.container}>
