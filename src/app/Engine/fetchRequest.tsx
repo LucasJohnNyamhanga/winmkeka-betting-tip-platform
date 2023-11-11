64f7b1a22c9ca9b73284da79e019b7e0cedd6fa4
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2023-11-11 08:57:29 +0300

    Update fetchRequest.tsx

--- src/app/Engine/fetchRequest.tsx
+++ src/app/Engine/fetchRequest.tsx
@@ -7,8 +7,6 @@ async function fetchData() {
     `https://apiv3.apifootball.com/?action=get_events&from=${today}&to=${today}&APIkey=${apiKey}`,
     { next: { revalidate: 900 } }
   );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
